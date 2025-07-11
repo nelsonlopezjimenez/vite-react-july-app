4d4aae2e480fef5a132ab3fe61e4a46dbed8e42b
Local Epsilon User <localepsilon@cis0678>
2025-07-11 16:55:48 -0700

    lifting state up: onSquareClick=() arrow handleClick(number)

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,47 @@
 // ---------- App.jsx setup for the tutorial
 
 import { useState } from "react";
+import './App.css'
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    console.log("clicked");
-    setValue('X');
-  }
+function Square({value, handleClick}) {
+  const onSquareClick = handleClick;
+  // const [value, setValue] = useState(null);
+  // --------- handleClick implementation was here but parent was not aware of it
+  // --------- now its reference need to be passed as a prop from Board
+  
   return (
-    <button className="square" onClick={handleClick}>{value}</button>
+    <button className="square" onClick={onSquareClick}>{value}</button>
+    // <button className="square" >{value}</button>
   )
 }
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    console.log("clicked", i.target);
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]}  handleClick={() => handleClick(0)}/>
+        <Square value={squares[1]}  handleClick={() => handleClick(1)}/>
+        <Square value={squares[2]}  handleClick={() => handleClick(2)}/>
       </div>
-      
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]}  handleClick={() =>handleClick(3)}/>
+        <Square value={squares[4]}  handleClick={() =>handleClick(4)}/>
+        <Square value={squares[5]}  handleClick={() =>handleClick(5)}/>
       </div>
-      
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]}  handleClick={() => handleClick(6)}/>
+        <Square value={squares[7]}  handleClick={() => handleClick(7)}/>
+        <Square value={squares[8]}  handleClick={() => handleClick(8)}/>
+          {/* instead of value={squares[0] onSquareClick={handleClick(0)}} which leads to infinite loop */}
       </div>
-      
     </>
   )
 }
@@ -41,3 +49,10 @@ function Board() {
 export default function App() {
   return <Board />
 }
+
+//**
+// Closures: Lexical scoping: init(){var name; displayName() {console.log(name)} displayName()} init()
+// outer function init
+// inner function displayName available only within init, it can reference its sorrounding state (lexical environment)
+// 
+//  */
