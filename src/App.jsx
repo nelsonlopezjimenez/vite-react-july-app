dc758854699ec55adfdfc360332552085a766f1c
Local Epsilon User <localepsilon@cis0678>
2025-07-11 11:53:19 -0700

    tictactoe passing data through props

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+// ---------- App.jsx setup for the tutorial
+function Square({ v }) {
+  return <button className="square">{v}</button>
+}
+function Board() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="board-row">
+        <Square v='1'/>
+        <Square v='2'/>
+        <Square v='3'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="board-row">
+        <Square v='4'/>
+        <Square v='5'/>
+        <Square v='6'/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="board-row">
+        <Square v='7'/>
+        <Square v='8'/>
+        <Square v='9'/>
+      </div>
     </>
   )
 }
 
-export default App
+export default function App() {
+  return <Board />
+}
